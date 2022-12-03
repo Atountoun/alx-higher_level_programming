3568d825b45767b3f18bf66eff1041d90a6f613f
Atountoun <tilltates8@gmail.com>
2022-12-03 02:40:01 +0000

    Fourth project

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int count_nodes(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * count_nodes - counts the number of nodes of the list
+ * @head: points to the first node of the list
+ *
+ * Return: the number of nodes
+ */
+int count_nodes(listint_t **head)
+{
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is
+ * palindrome
+ * @head: points to the first node of the list
+ *
+ * Return: 0 if not palindromic, 1 else
+ */
+int is_palindrome(listint_t **head)
+{
+	return (1);
+}
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
