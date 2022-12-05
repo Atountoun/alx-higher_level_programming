661e4fdb554963d707055e5b6daaa98bd3f78803
Atountoun <tilltates8@gmail.com>
2022-12-05 21:17:16 +0000

    Tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    len_ta = len(tuple_a)
-    len_tb = len(tuple_b)
+    # This function can be more adaptable for tuple of many elements
+    
+    len_ta = len(tuple_a) if len(tuple_a) < 2 else 2
+    len_tb = len(tuple_b) if len(tuple_b) < 2 else 2
     len_min_tu = len_ta if len_ta < len_tb else len_tb
     max_tu = tuple_a if len_ta > len_tb else tuple_b
     result = []
@@ -11,7 +12,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(len_min_tu):
         result.append(tuple_a[i] + tuple_b[i])
 
-    for i in max_tu[len_min_tu:]:
+    for i in max_tu[len_min_tu : 2]:
         result.append(i)
 
-    return tuple(result)
+    return (tuple(result))
