f45158cef2a6b744dcbbb32cc988bd973e3476a7
Atountoun <tilltates8@gmail.com>
2023-04-08 06:41:35 -0700

    Replace --json by -d

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request. The data is contained in a file.
-curl -s -X POST "$1" --json @"$2"
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST request to the passed URL arg
-curl -s -X POST "$1" --data-urlencode "email=test%40gmail.com"  --data-urlencode "subject=I will always be there for PLD"
+curl -s -X POST "$1" --data-urlencode "email=test@gmail.com"  --data-urlencode "subject=I will always be there for PLD"
