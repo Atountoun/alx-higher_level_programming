c086955dee575bddd821b5abd0d8a65e704e8699
Atountoun <tilltates8@gmail.com>
2023-04-08 04:57:56 -0700

    Interview question

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script that takes 2 arguments in order to solve this challenge.
+- First argument with be the `repository name`
+- Second argument will be the `owner name`
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+#    url = "https://api.github.com/repos/OWNER/REPO/commits"
+
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            print(f"{commit.get('sha')} {commit.get('commit').get('author').get('name')}")
+    else:
+        print(None)
